355468ef35043fa88cfc310ef3411bc420404500
Vasil Karpliuk <bus.go.it@gmail.com>
2022-09-25 12:00:37 +0300

    task 1 init

---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,45 @@
+<div class="profile">
+  <div class="description">
+    <img
+      src="https://cdn-icons-png.flaticon.com/512/1077/1077012.png"
+      alt="User avatar"
+      class="avatar"
+    />
+    <p class="name">Petra Marica</p>
+    <p class="tag">@pmarica</p>
+    <p class="location">Salvador, Brasil</p>
+  </div>
+
+  <ul class="stats">
+    <li>
+      <span class="label">Followers</span>
+      <span class="quantity">1000</span>
+    </li>
+    <li>
+      <span class="label">Views</span>
+      <span class="quantity">2000</span>
+    </li>
+    <li>
+      <span class="label">Likes</span>
+      <span class="quantity">3000</span>
+    </li>
+  </ul>
+</div>;
+
+// Компонент должен принимать несколько пропсов с информацией о пользователе:
+
+// username — имя пользователя
+// tag — тег в социальной сети без @
+// location — город и страна
+// avatar — ссылка на изображение
+// stats — объект с информацией об активности
+
+// import user from 'path/to/user.json;
+
+// <Profile
+//   username={user.username}
+//   tag={user.tag}
+//   location={user.location}
+//   avatar={user.avatar}
+//   stats={user.stats}
+// />
