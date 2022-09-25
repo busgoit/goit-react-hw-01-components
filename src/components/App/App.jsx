3c44206f4c258ad154697d9a6afb387d385d2e9e
busgoit <bus.go.it@gmail.com>
2022-09-25 13:06:04 +0300

    task 1 init

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,16 +1,22 @@
+import Profile from '../components';
+
 export const App = () => {
   return (
-    <div
+    <div>
+      <Profile />
+    </div>
+  );
+};
+
+/* <div
       style={{
         height: '100vh',
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
       React homework template
-    </div>
-  );
-};
+</div> */
