d9327c537a42866f8b88f5c9e85f559503432894
Vasil Karpliuk <bus.go.it@gmail.com>
2022-09-26 22:22:22 +0300

    finished task 1

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,5 @@
-import user from '../../data/user.json';
-import Profile from '../components';
+import user from 'data/user.json';
+import Profile from 'components/components';
 
 export const App = () => {
   return (
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,5 +1,6 @@
 import PropTypes from 'prop-types';
-import defaultImg from '../../img/no-image-icon-2.jpg';
+import defaultImg from 'img/no-image-icon-2.jpg';
+import css from './Profile.module.css';
 
 const Profile = ({
   username,
@@ -9,26 +10,26 @@ const Profile = ({
   stats: { followers, views, likes },
 }) => {
   return (
-    <div class="profile">
-      <div class="description">
-        <img src={avatar} alt={username} class="avatar" />
-        <p class="name">{username}</p>
-        <p class="tag">@{tag}</p>
-        <p class="location">{location}</p>
+    <div className={css.profile}>
+      <div className={css.description}>
+        <img src={avatar} alt={username} className={css.avatar} />
+        <p className={css.name}>{username}</p>
+        <p className={css.tag}>@{tag}</p>
+        <p className={css.location}>{location}</p>
       </div>
 
-      <ul class="stats">
-        <li>
-          <span class="label">Followers: </span>
-          <span class="quantity">{followers}</span>
+      <ul className={css.stats}>
+        <li className={css.stat}>
+          <span className={css.label}>Followers</span>
+          <span className={css.quantity}>{followers}</span>
         </li>
-        <li>
-          <span class="label">Views: </span>
-          <span class="quantity">{views}</span>
+        <li className={css.stat}>
+          <span className={css.label}>Views</span>
+          <span className={css.quantity}>{views}</span>
         </li>
-        <li>
-          <span class="label">Likes: </span>
-          <span class="quantity">{likes}</span>
+        <li className={css.stat}>
+          <span className={css.label}>Likes</span>
+          <span className={css.quantity}>{likes}</span>
         </li>
       </ul>
     </div>
